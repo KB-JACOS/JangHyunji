e9d635f24371fc0c892f7ad63baf655a2964712c
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-04-29 10:19:48 +0900

    solved: baekjoon_11268 - 절댓값 힙

---
+++ baekjoon_11268.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class baekjoon_11268 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int abs1 = Math.abs(o1);
+            int abs2 = Math.abs(o2);
+
+            return abs1 == abs2 ? Integer.compare(o1, o2) : Integer.compare(abs1, abs2);
+        });
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(x);
+            }
+        }
+        br.close();
+        System.out.println(sb);
+    }
+}
