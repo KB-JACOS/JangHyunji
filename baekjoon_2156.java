5e7323ff8e8273b1f8ba32358d2b3f2086ed91e7
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-06-17 09:15:45 +0900

    baekjoon_2156 - 포도주 시식

---
+++ baekjoon_2156.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class baekjoon_2156 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] wine = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = wine[1];
+        if (N > 1) {
+            dp[2] = wine[1] + wine[2];    
+        }
+        
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-3] + wine[i-1] + wine[i], dp[i-2] + wine[i]));
+        }
+
+        System.out.println(dp[N]);
+    }
+}
